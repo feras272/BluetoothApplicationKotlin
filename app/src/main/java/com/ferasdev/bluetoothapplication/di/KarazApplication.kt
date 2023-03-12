37500c36cbd397b8b384a412039b89974acaee13
feras alnowiser <ferasdev@gmail.com>
2023-03-13 00:14:08 +0300

    add: Hilt Android Application class KarazApplication.kt

---
+++ app/src/main/java/com/ferasdev/bluetoothapplication/di/KarazApplication.kt
@@ -0,0 +1,8 @@
+package com.ferasdev.bluetoothapplication.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class KarazApplication: Application() {
+}
