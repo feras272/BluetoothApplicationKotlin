b899c97c419a930f847b5af68fc2e97cce37ffd4
feras alnowiser <ferasdev@gmail.com>
2023-03-13 00:17:45 +0300

    add: AppModule.kt object and provideUserRepository function dependency to inject the other classes that depends on this function which return UserRepository

---
+++ app/src/main/java/com/ferasdev/bluetoothapplication/di/AppModule.kt
@@ -0,0 +1,47 @@
+package com.ferasdev.bluetoothapplication.di
+
+import android.content.Context
+import com.ferasdev.bluetoothapplication.R
+import com.ferasdev.bluetoothapplication.data.repositories.UserRepository
+import com.ferasdev.bluetoothapplication.data.repositories.UserRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    // Here we instantiate all the instances we are using in our application
+    // Instantiate once and using it across the application
+
+    // @Named("firstName")
+//    @Inject
+//    lateinit var userRepository: UserRepositoryImpl
+
+    @Singleton
+    @Provides
+    @Named("FirstName")
+    fun provideGetFirstName(@ApplicationContext context: Context): String {
+        return context.getString(R.string.firstName)
+    }
+
+    @Singleton
+    @Provides
+    @Named("SecondName")
+    fun provideGetSecondName(@ApplicationContext context: Context): String { // to access the context inside the app module
+        return context.getString(R.string.secondName)
+    }
+
+    @Singleton
+    @Provides
+    fun provideUserRepository(): UserRepository = UserRepositoryImpl()
+
+
+
+}
