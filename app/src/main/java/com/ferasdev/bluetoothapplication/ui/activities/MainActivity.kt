1f2dd060d4b5596afc4df7b7ba59192d87c5d12b
feras alnowiser <ferasdev@gmail.com>
2023-03-13 09:57:50 +0300

    add: dependency injection with variable and log the output

--- app/src/main/java/com/ferasdev/bluetoothapplication/ui/activities/MainActivity.kt
+++ app/src/main/java/com/ferasdev/bluetoothapplication/ui/activities/MainActivity.kt
@@ -7,19 +7,29 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModel
 import com.ferasdev.bluetoothapplication.R
 import com.ferasdev.bluetoothapplication.databinding.ActivityMainBinding
 import androidx.navigation.NavController
+import androidx.navigation.Navigator
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.ferasdev.bluetoothapplication.viewmodel.MainViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import javax.inject.Named
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -29,12 +39,25 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bluetoothAdapter: BluetoothAdapter
     private val REQUEST_ENABLE_BT = 3
 
+    val mainViewModel: MainViewModel by viewModels()
+
+    // @Named("getFirstName")
+
+    @Inject
+    @Named("FirstName")
+    lateinit var firstName: String
+
+    var TAG = "MainActivityDaggerHilt"
+
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        Log.d(TAG, "Name is $firstName")
+        mainViewModel
 
         val btmNav: BottomNavigationView = binding.btmNavActivityMain
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.btm_nav_activity_main)
